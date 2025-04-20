f2c68446abe7b1e09aefdeb611b9c50e0f17ba6d
Sanjyot Sathe <ssathe1@charlotte.edu>
2025-04-20 13:38:02 -0400

    test

--- innsight_ui/src/api/aiAPi.tsx
+++ innsight_ui/src/api/aiAPi.tsx
@@ -13,9 +13,78 @@ async function hotel_summarize() {
       ],
       model: "llama-3.3-70b-versatile",
     })
-  console.log(completion.choices[0].message.content);
-  return completion.choices[0].message.content;
+  const response = completion.choices[0].message.content;
+  const cleaned = response.replace(/```json|```/g, '').trim();
+
+// Step 2: Parse JSON
+const result = JSON.parse(cleaned);
+
+console.log(result.name);
+return result.name;
+}
+
+async function occupancy_chart() {
+  const completion = await groq.chat.completions
+    .create({
+      messages: [
+        {
+          role: "user",
+          content: "You are now a hotel business helper. I will give you location of the hotel and details like star rating and max room occupancy. You will provide me data of approx occupancy in the hotel according to month. I want it in json format \n Location: Las Vegas \n Luxury: 3 star hotel \n Rooms: 2-6 people rooms",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    })
+  const response = completion.choices[0].message.content;
+  const cleaned = response.replace(/```json|```/g, '').trim();
+
+// Step 2: Parse JSON
+const result = JSON.parse(cleaned);
+
+console.log(result.name);
+return result.name;
+}
+
+async function revenue() {
+  const completion = await groq.chat.completions
+    .create({
+      messages: [
+        {
+          role: "user",
+          content: "You are helping me with my hotel business project. I will provide you with the location, luxury rating and room size for the hotel. You need to help me get data for the revenue of the hotel month-wise. I want it in json format, with no additional textual content \n Location: Las Vegas \n Luxury: 3 star hotel \n Rooms: 2-6 people rooms",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    })
+  const response = completion.choices[0].message.content;
+  const cleaned = response.replace(/```json|```/g, '').trim();
+
+// Step 2: Parse JSON
+const result = JSON.parse(cleaned);
+
+console.log(result.name);
+return result.name;
+}
+
+async function inquiry() {
+  const completion = await groq.chat.completions
+    .create({
+      messages: [
+        {
+          role: "user",
+          content: "You are helping me with my hotel business project. I will provide you with the location, luxury rating and room size for the hotel. You need to help me get data for the no of inquiries of the hotel month-wise. I want it in json format, with no additional textual content \n Location: Las Vegas \n Luxury: 3 star hotel \n Rooms: 2-6 people rooms",
+        },
+      ],
+      model: "llama-3.3-70b-versatile",
+    })
+  const response = completion.choices[0].message.content;
+  const cleaned = response.replace(/```json|```/g, '').trim();
+
+// Step 2: Parse JSON
+const result = JSON.parse(cleaned);
+
+console.log(result.name);
+return result.name;
 }
 
 
-export default hotel_summarize;
+export default {hotel_summarize, occupancy_chart, inquiry, revenue};
